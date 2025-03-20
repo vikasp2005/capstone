9bae226b05936ab774799855aeed6b2fce7604a3
vikas <119327799+vikasp2005@users.noreply.github.com>
2025-03-20 14:09:19 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@
     docker login -u pvikivikas5 -p dckr_pat_2AYIcTrq89hnWg7-K_5Lrm0Cqks
     docker tag test1 pvikivikas5/devops
     docker push pvikivikas5/devops
-    #docker-compose up -d
+    docker-compose up -d
     
