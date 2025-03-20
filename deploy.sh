cbb387edb08e526bbb92989c429155d5dfec8bee
vikas <119327799+vikasp2005@users.noreply.github.com>
2025-03-20 13:25:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u pvikivikas5 -p dckr_pat_fvUY7Gp-NRcpFclcCgY1cMbv11g
+    docker login -u pvikivikas5 -p dckr_pat_2AYIcTrq89hnWg7-K_5Lrm0Cqks
     docker tag test1 pvikivikas5/devops
     docker push pvikivikas5/devops
     #docker-compose up -d
