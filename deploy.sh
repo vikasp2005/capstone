10b941760e61a49b064368121d4f72f45ae0ff2d
vikas <119327799+vikasp2005@users.noreply.github.com>
2025-03-20 11:38:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u pvikivikas5 -p dckr_pat_fvUY7Gp-NRcpFclcCgY1cMbv11g
-    docker tag test pvikivikas5/devops
+    docker tag test1 pvikivikas5/devops
     docker push pvikivikas5/devops
     #docker-compose up -d
     
